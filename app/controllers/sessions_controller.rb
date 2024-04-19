7025ba32575281f0b9d84ee9edbfa908a3ca32f7
gabgaleski <galeski35@gmail.com>
2024-04-19 14:45:17 -0300

    feat: Implementa autenticação do login e criação de conta

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  
+  def new
+  end
+
+  def create
+    user = User.find_by(username: params[:username])
+
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Logged in successfully"
+    else
+      flash[:alert] = "Invalid username or password"
+      render :new
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
+  root 'main#index'
+
   resources :posts do 
     resources :comments
   end
+  
+  resources :users, only: [:new, :create, :show, :edit, :update, :destroy]
+
+  get '/', to: 'posts#index'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  
+  post '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
+
 end
