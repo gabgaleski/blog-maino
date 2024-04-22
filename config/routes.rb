97c740f15c0efc4aad15bf37a83fabe8b2cd29c8
gabgaleski <galeski35@gmail.com>
2024-04-22 16:18:37 -0300

    feat: Implementa internacionalização

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,32 @@
 Rails.application.routes.draw do
-  root 'main#index'
+  scope "(:locale)", locale: /en|es/ do
+    get '/:locale' => 'dashboard#index' 
 
-  resources :posts do 
-    resources :comments
+
+    root 'main#index'
+
+    resources :posts do 
+      resources :comments
+    end
+  
+    post '/users', to: 'users#create'
+    get '/users', to: 'users#new'
+
+    get '/profile', to: 'profiles#edit'
+    patch '/profile', to: 'profiles#update'
+
+    get '/', to: 'posts#index'
+
+    get '/login', to: 'sessions#new'
+    post '/login', to: 'sessions#create'
+  
+    delete'/logout', to: 'sessions#destroy'
+
+    get '/password/reset/edit', to: 'password_resets#edit'
+    post '/password/reset/edit', to: 'password_resets#update'
+
+    get '/password/reset', to: 'password_resets#new'
+    post '/password/reset', to: 'password_resets#create'
+
   end
-  
-  get '/users', to: 'users#new'
-  post '/users', to: 'users#create'
-
-  get '/profile', to: 'profiles#edit'
-  patch '/profile', to: 'profiles#update'
-
-  get '/', to: 'posts#index'
-
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  
-  delete'/logout', to: 'sessions#destroy'
-
-  get '/password/reset/edit', to: 'password_resets#edit'
-  post '/password/reset/edit', to: 'password_resets#update'
-
-  get '/password/reset', to: 'password_resets#new'
-  post '/password/reset', to: 'password_resets#create'
-
-
 end
